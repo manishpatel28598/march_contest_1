3718270f2fe7f942c1b2eb1ef02d7afccd0263ed
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-16 16:52:30 +0530

    making some improvement in clear button

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const incrementButton = document.getElementById("increment");
 const countDisplay = document.getElementById("count-display");
 const clearButton = document.getElementById("clear");
 const errorMessage = document.getElementById("error-message");
+// const btncontainer = document.getElementsByClassName("btn-container");
 
 let count = 0;
 
 decrementButton.addEventListener("click", () => {
   if (count > 0) {
     count--;
-    countDisplay.innerHTML = `Your Current Count is : ${count}`;
+    btncontainer.innerHTML = `Your Current Count is : ${count}`;
   } else {
     errorMessage.style.display = "block";
   }
@@ -19,12 +20,14 @@ incrementButton.addEventListener("click", () => {
   count++;
   countDisplay.innerHTML = `Your Current Count is : ${count}`;
   errorMessage.style.display = "none";
+  clearButton.style.display = "right";
 });
 
 clearButton.addEventListener("click", () => {
   count = 0;
   countDisplay.innerHTML = `Your Current Count is : ${count}`;
   errorMessage.style.display = "none";
+  clearButton.style.display = "none";
 });
 
 countDisplay.addEventListener("click", () => {
